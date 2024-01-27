25032927701d41600c407ae086ab1d3ee77bed2d
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-01-27 19:13:49 +0545

    manage env variables

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ connectToMongo();
 
 const app = express();
 
-const port = 5000;
-
 app.use(cors());
 app.use(express.json());
 
@@ -15,6 +13,8 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.listen(port, () => {
-  console.log(`inotebook backend listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(
+    `inotebook backend listening on port ${process.env.PORT || 5000}`
+  );
 });
